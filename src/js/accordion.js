4c843f165a604e8c5fd7059b252ce7d78d7299e9
Kateryna <katyaukr2014@gmail.com>
2026-04-05 21:29:25 +0300

    add

---
+++ src/js/accordion.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.faq-acc-el-trigger').forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const parentElement = trigger.closest('.faq-acc-el');
+        const panel = parentElement.querySelector('.faq-acc-el-descr-frame');
+        const svg = trigger.querySelector('svg');
+        const isOpen = parentElement.classList.contains('open');
+
+        const plusIcon = `
+<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M14 4V24" stroke="#AAD0FF" />
+  <path d="M24.0049 13.9951L4.00488 13.9951" stroke="#AAD0FF" />
+</svg>`;
+
+        const minusIcon = `
+<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M24.0049 13.9951L4.00488 13.9951" stroke="#AAD0FF" />
+</svg>`;
+
+        if (!isOpen) {
+            parentElement.classList.add('open');
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+            svg.outerHTML = minusIcon;
+        } else {
+            parentElement.classList.remove('open');
+            panel.style.maxHeight = '0';
+            svg.outerHTML = plusIcon;
+        }
+    });
+});
